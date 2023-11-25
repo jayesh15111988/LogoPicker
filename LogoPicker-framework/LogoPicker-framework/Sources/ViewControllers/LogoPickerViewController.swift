c55444257635ea710a24038c5bda4fba3b31ae4c
Jayesh Kawli <j.kawli@gmail.com>
2023-11-25 13:14:24 +0100

    Added dummy instructional labels

---
+++ LogoPicker-framework/LogoPicker-framework/Sources/ViewControllers/LogoPickerViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LogoPickerViewController.swift
+//  LogoPicker-framework
+//
+//  Created by Jayesh Kawli on 11/25/23.
+//
+
+import UIKit
+
+public class LogoPickerViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- LogoPicker-iOS/LogoPicker-iOS/ViewController.swift
+++ LogoPicker-iOS/LogoPicker-iOS/ViewController.swift
@@ -12,9 +12,27 @@ typealias ViewModel = LogoView.ViewModel
 
 class ViewController: UIViewController {
 
+    private enum Constants {
+        static let horizontalSpacing: CGFloat = 10.0
+    }
+
     private let logoView: LogoView = {
         let logoView = LogoView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 50, height: 50)))
         return logoView
+    }()
+
+    private let logoTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 13)
+        label.textColor = .black
+        return label
+    }()
+
+    private let logoSubtitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.textColor = .darkGray
+        return label
     }()
 
     override func viewDidLoad() {
@@ -23,12 +41,29 @@ class ViewController: UIViewController {
     }
 
     private func setupViews() {
-        
-        logoView.delegate = self
 
+        logoView.delegate = self
+        logoTitleLabel.text = "Icon"
+        logoSubtitleLabel.text = "Tap to Change Icon"
+
+        self.view.addSubview(logoTitleLabel)
+        self.view.addSubview(logoSubtitleLabel)
         self.view.addSubview(logoView)
-        logoView.center = CGPoint(x: self.view.bounds.midX, y: self.view.bounds.midY)
+
+        logoView.frame.origin = CGPoint(x: Constants.horizontalSpacing, y: self.view.bounds.midY - logoView.frame.height / 2.0)
         logoView.configure(with: ViewModel(logoState: .title(initials: "JK"), backgroundColor: .blue, foregroundColor: .white, logoContentMode: .scaleAspectFit))
+
+        let titlesHorizontalSpacing = Constants.horizontalSpacing * 2 + logoView.frame.width
+        let titlesWidth = self.view.frame.width - 2 * Constants.horizontalSpacing - Constants.horizontalSpacing - logoView.frame.width
+        let titlesHeight = logoView.frame.height / 4.0
+        let yOffsetFromCenter = logoView.frame.height / 4.0
+
+        logoTitleLabel.frame = CGRect(origin: CGPoint(x: titlesHorizontalSpacing, y: self.logoView.center.y - yOffsetFromCenter), size: CGSize(width: titlesWidth, height: titlesHeight))
+
+        logoSubtitleLabel.frame = CGRect(origin: CGPoint(x: titlesHorizontalSpacing, y: self.logoView.center.y), size: CGSize(width: titlesWidth, height: titlesHeight))
+
+        logoTitleLabel.frame.origin.y -= 5
+        logoSubtitleLabel.frame.origin.y += 5
     }
 }
 
