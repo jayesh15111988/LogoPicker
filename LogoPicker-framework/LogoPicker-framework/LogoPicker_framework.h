57ce7921787a498d7b04a4ed20fe416a3a732392
Jayesh Kawli <j.kawli@gmail.com>
2023-11-25 11:19:46 +0100

    Completed initial set up of workspace, framework and demo project

---
+++ LogoPicker-framework/LogoPicker-framework/LogoPicker_framework.h
@@ -0,0 +1,18 @@
+//
+//  LogoPicker_framework.h
+//  LogoPicker-framework
+//
+//  Created by Jayesh Kawli on 11/25/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LogoPicker_framework.
+FOUNDATION_EXPORT double LogoPicker_frameworkVersionNumber;
+
+//! Project version string for LogoPicker_framework.
+FOUNDATION_EXPORT const unsigned char LogoPicker_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LogoPicker_framework/PublicHeader.h>
+
+
---
+++ LogoPicker-iOS/LogoPicker-iOS/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  LogoPicker-iOS
+//
+//  Created by Jayesh Kawli on 11/25/23.
+//
+
+import UIKit
+import LogoPicker_framework
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
